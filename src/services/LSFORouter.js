6f4f5c382362c8bcc612bf63ce8c7a8428e0b910
jktheartheist <jk475767@gmail.com>
2025-04-03 12:37:49 +0530

    Revert "Add files via upload"
    This reverts commit 02b7453e836c22f3fd86643ef0b4641e048ae733.

--- src/services/LSFORouter.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import LSFOController from '../controllers/LSFOController.js';
-const LSFORouter = express.Router();
-
-LSFORouter.post('/',LSFOController.createRecord);
-LSFORouter.get('/all',LSFOController.fetchRecords);
-
-export default LSFORouter;
--- src/services/bwrRouter.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import bwrController from '../controllers/bwrController.js';
-const bwrRouter = express.Router();
-bwrRouter.post('/',bwrController.createRecord);
-bwrRouter.get('/all',bwrController.fetchRecords);
-export default bwrRouter;
